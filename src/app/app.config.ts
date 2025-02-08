9a4f1f7bde2007410d70350bc53034712e0e3d5b
ianer-glitch <ianrepker@gmail.com>
2025-02-08 12:13:02 -0300

    installed primng and prime icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
 
 import { routes } from './app.routes';
+import FridgeTheme from './theme.preset';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+          preset: FridgeTheme,
+          options: {
+            darkModeSelector: ''
+        }
+      }
+  })
+  ]
 };
+
+
+
---
+++ src/app/theme.preset.ts
@@ -0,0 +1,23 @@
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
+
+const FridgeTheme = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{yellow.50}',
+            100: '{yellow.100}',
+            200: '{yellow.200}',
+            300: '{yellow.300}',
+            400: '{yellow.400}',
+            500: '{yellow.500}',
+            600: '{yellow.600}',
+            700: '{yellow.700}',
+            800: '{yellow.800}',
+            900: '{yellow.900}',
+            950: '{yellow.950}'
+        }
+    }
+    
+});
+
+export default FridgeTheme;
