8fc856344aae4ca8a48778166784ef93c00c5574
ianer-glitch <ianrepker@gmail.com>
2025-02-09 06:48:05 -0300

    created footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { PrimeIconComponent } from './shared/components/atoms/prime-icon/prime-icon.component';
+import { FooterComponent } from "./shared/components/molecules/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ButtonModule,PrimeIconComponent],
+  imports: [RouterOutlet, ButtonModule, PrimeIconComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/shared/components/molecules/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PrimeIconComponent } from "../../atoms/prime-icon/prime-icon.component";
+
+@Component({
+  selector: 'app-footer',
+  imports: [PrimeIconComponent],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
