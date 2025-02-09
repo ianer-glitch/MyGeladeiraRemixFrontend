25f33f158643ff2a6fdf4fba3fe72e3ca6b572d0
ianer-glitch <ianrepker@gmail.com>
2025-02-09 08:05:48 -0300

    initial pwa config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -6,6 +6,7 @@ import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 import FridgeTheme from './theme.preset';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 export const appConfig: ApplicationConfig = {
@@ -20,7 +21,10 @@ export const appConfig: ApplicationConfig = {
             darkModeSelector: ''
         }
       }
-  })
+  }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
 
