7debfd276150fe8ba1c1bf4b1dc7beb869e63656
ianer-glitch <ianrepker@gmail.com>
2025-02-09 07:20:52 -0300

    added footer

---
+++ src/app/shared/components/atoms/ads-container/ads-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ads-container',
+  imports: [],
+  templateUrl: './ads-container.component.html',
+  styleUrl: './ads-container.component.css'
+})
+export class AdsContainerComponent {
+
+}
--- src/app/shared/components/molecules/footer/footer.component.ts
+++ src/app/shared/components/molecules/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PrimeIconComponent } from "../../atoms/prime-icon/prime-icon.component";
+import { AdsContainerComponent } from "../../atoms/ads-container/ads-container.component";
 
 @Component({
   selector: 'app-footer',
-  imports: [PrimeIconComponent],
+  imports: [PrimeIconComponent, AdsContainerComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
