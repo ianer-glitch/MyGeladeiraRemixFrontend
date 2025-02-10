dcc4664521db2a9204bf955861107a21d7fead83
ianer-glitch <ianrepker@gmail.com>
2025-02-10 18:38:19 -0300

    implemented formAcessor on input-text

--- src/app/shared/components/atoms/input-text/input-text.component.ts
+++ src/app/shared/components/atoms/input-text/input-text.component.ts
@@ -1,14 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, NgModel } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'input-text',
-  imports: [InputTextModule],
+  imports: [InputTextModule,FormsModule],
   templateUrl: './input-text.component.html',
-  styleUrl: './input-text.component.css'
+  styleUrl: './input-text.component.css',
+  providers:[
+    {
+      provide:NG_VALUE_ACCESSOR,
+      useExisting:forwardRef(()=>InputTextComponent),
+      multi:true
+    }
+  ]
 })
-export class InputTextComponent {
-@Input() id:string = ""
-@Input() label:string = ""
-@Input() fluid:boolean = false
+export class InputTextComponent implements ControlValueAccessor {
+  
+  onChange : (param:any)=>void = ()=>{}
+  onTouch : ()=>void = ()=>{}
+
+  writeValue(obj: any): void {
+    this.text=obj
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch=fn
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  @Input() id:string = ""
+  @Input() label:string = ""
+  @Input() fluid:boolean = false
+  @Input() placeholder:string = ""
+  text:string = ""
+  
+  handleChange(newVal:string){
+    this.text=newVal
+    this.onChange(newVal)
+  }
 }
