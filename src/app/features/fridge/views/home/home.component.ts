b4ede981c46300d29383422ba5877baa41dd2a91
ianer-glitch <ianrepker@gmail.com>
2025-02-20 05:39:10 -0300

    created home

--- src/app/features/fridge/views/home/home.component.ts
+++ src/app/features/fridge/views/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../../../shared/components/molecules/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
