19e2374b5d9713c7a77e2e5b35f1a619abb3cb19
ianer-glitch <ianrepker@gmail.com>
2025-02-20 18:25:48 -0300

    conclue expirated-item-selector

--- src/app/features/fridge/components/expiration-time-selector/expiration-time-selector.component.ts
+++ src/app/features/fridge/components/expiration-time-selector/expiration-time-selector.component.ts
@@ -1,26 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 import { ChipComponent } from "../../../../shared/components/atoms/chip/chip.component";
 import { PrimeIconComponent } from "../../../../shared/components/atoms/prime-icon/prime-icon.component";
-import { ControlValueAccessor } from '@angular/forms';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DatePickerModule } from 'primeng/datepicker';
+import { PopupComponent } from "../../../../shared/components/atoms/popup/popup.component";
+import { ButtonComponent } from "../../../../shared/components/atoms/button/button.component";
 
 @Component({
   selector: 'expiration-time-selector',
-  imports: [ChipComponent, PrimeIconComponent],
+  imports: [
+    ChipComponent,
+    PrimeIconComponent,
+    CommonModule,
+    DatePickerModule,
+    FormsModule,
+    PopupComponent,
+    ButtonComponent
+],
   templateUrl: './expiration-time-selector.component.html',
-  styleUrl: './expiration-time-selector.component.css'
+  styleUrl: './expiration-time-selector.component.css',
+  providers:[
+      {
+        provide:NG_VALUE_ACCESSOR,
+        useExisting:forwardRef(()=>ExpirationTimeSelectorComponent),
+        multi:true
+      }
+    ]
 })
 export class ExpirationTimeSelectorComponent implements ControlValueAccessor {
+  @Input() value : Date = new Date()
+  showDatePicker:boolean = false
+   
+  onChange : (param:any)=>void = ()=>{}
+  onTouch : ()=>void = ()=>{}
+
   writeValue(obj: any): void {
-    throw new Error('Method not implemented.');
+    this.value = obj
   }
   registerOnChange(fn: any): void {
-    throw new Error('Method not implemented.');
+    this.onChange = fn
   }
   registerOnTouched(fn: any): void {
-    throw new Error('Method not implemented.');
+    this.onTouch = fn
   }
   setDisabledState?(isDisabled: boolean): void {
     throw new Error('Method not implemented.');
+  }
+
+  handleAddDate(days : number) : Date{
+    let currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + days);
+
+    currentDate.setHours(0)
+    currentDate.setMinutes(0)
+    currentDate.setSeconds(0)
+    currentDate.setMilliseconds(0)
+    
+    return currentDate
+  }
+
+
+  handleClick(days : number){
+    
+    this.value = this.handleAddDate(days)
+    this.onChange(days)
+  }
+
+  isActive(days : number){
+    return this.value.getTime() === this.handleAddDate(days).getTime()
   }
 
 }
