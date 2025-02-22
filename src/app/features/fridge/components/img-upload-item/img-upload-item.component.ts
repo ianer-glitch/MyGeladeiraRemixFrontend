82e30388ac280cff4e2228714bcb81170f65fcdd
ianer-glitch <ianrepker@gmail.com>
2025-02-22 06:12:50 -0300

    changes in image-item-component

--- src/app/features/fridge/components/img-upload-item/img-upload-item.component.ts
+++ src/app/features/fridge/components/img-upload-item/img-upload-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
@@ -18,7 +18,8 @@ export class ImgUploadItemComponent implements ControlValueAccessor{
   onChange : (param:any)=>void = ()=>{}
   onTouch : ()=>void = ()=>{}
   value:File = {} as File
-  fileUrl = ""
+  @Input() fileUrl:string = ""
+  isDisabled:boolean = false
 
   writeValue(obj: any): void {
     this.value = obj
@@ -30,30 +31,33 @@ export class ImgUploadItemComponent implements ControlValueAccessor{
     this.onTouch = fn
   }
   setDisabledState?(isDisabled: boolean): void {
-    
+    this.isDisabled = isDisabled
   }
  
   
   handleImageUpload(){
-    const fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.style.display = 'none'; 
+    if(!this.isDisabled){
+      const fileInput = document.createElement('input');
+      fileInput.type = 'file';
+      fileInput.style.display = 'none'; 
+  
+      document.body.appendChild(fileInput);
+  
+      fileInput.click();
+  
+      fileInput.addEventListener('change', (event : any) => {
+        const input = event.target as HTMLInputElement;
+        
+        if (input && input.files) {
+          this.value = input.files[0]; 
+          this.onChange(input.files[0])
+          this.fileUrl = this.convertFileToURL(input.files[0])
+        }
+      });
+  
+      fileInput.remove();
 
-    document.body.appendChild(fileInput);
-
-    fileInput.click();
-
-    fileInput.addEventListener('change', (event : any) => {
-      const input = event.target as HTMLInputElement;
-      
-      if (input && input.files) {
-        this.value = input.files[0]; 
-        this.onChange(input.files[0])
-        this.fileUrl = this.convertFileToURL(input.files[0])
-      }
-    });
-
-    fileInput.remove();
+    }
   }
 
   convertFileToURL(file: File): string {
