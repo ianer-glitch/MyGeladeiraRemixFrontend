7a3bed57f1c0b579be91f5cc01ee03fac37a6245
ianer-glitch <ianrepker@gmail.com>
2025-02-27 15:57:00 -0300

    fisnish sidebar

--- src/app/shared/components/molecules/footer/footer.component.ts
+++ src/app/shared/components/molecules/footer/footer.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { PrimeIconComponent } from "../../atoms/prime-icon/prime-icon.component";
 import { AdsContainerComponent } from "../../atoms/ads-container/ads-container.component";
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
-  imports: [PrimeIconComponent, AdsContainerComponent, SidebarComponent],
+  imports: [PrimeIconComponent, AdsContainerComponent, SidebarComponent,FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  visible:boolean = false
 }
--- src/app/shared/components/molecules/sidebar/sidebar.component.ts
+++ src/app/shared/components/molecules/sidebar/sidebar.component.ts
@@ -1,25 +1,49 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, forwardRef, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { DrawerModule } from 'primeng/drawer';
 import { PrimeIconComponent } from "../../atoms/prime-icon/prime-icon.component";
 import { LocalStorageService } from '../../../../core/services/local-storage/local-storage.service';
 import { ToastService } from '../../../../core/services/toast/toast.service';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'sidebar',
   imports: [DrawerModule, CommonModule, PrimeIconComponent],
   encapsulation:ViewEncapsulation.None,
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
+  providers:[
+      {
+        provide:NG_VALUE_ACCESSOR,
+        useExisting:forwardRef(()=>SidebarComponent),
+        multi:true
+      }
+    ]
 })
-export class SidebarComponent {
+export class SidebarComponent implements ControlValueAccessor {
   visible:boolean = true
   
   constructor(private router : Router,
     private localStorageService:LocalStorageService,
     private toastService:ToastService,
   ) {
+  }
+
+  onChange : (param:any)=>void = ()=>{}
+  onTouch : ()=>void = ()=>{}
+
+  writeValue(obj: any): void {
+    this.visible=obj
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch=fn
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    
   }
 
 
@@ -35,5 +59,10 @@ export class SidebarComponent {
     this.localStorageService.clear()
     this.router.navigate(["/auth/login"])
     this.toastService.showSucces("Você saiu!")
+  }
+
+  handleChange(newVal:boolean){
+    this.visible = newVal
+    this.onChange(newVal)
   }
 }
