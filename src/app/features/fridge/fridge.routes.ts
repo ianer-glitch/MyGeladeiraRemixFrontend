93a2e5f0b973c513acfaa8ffa43695a0a131529b
ianer-glitch <ianrepker@gmail.com>
2025-03-08 07:44:48 -0300

    created recipes page

--- src/app/features/fridge/fridge.routes.ts
+++ src/app/features/fridge/fridge.routes.ts
@@ -6,11 +6,13 @@ import { EditItemComponent } from './views/edit-item/edit-item.component';
 import { ShoppingListComponent } from './views/shopping-list/shopping-list.component';
 import { inject } from '@angular/core';
 import AdminGuard from '../../core/guards/admin/AdminGuard';
+import { RecipesComponent } from './views/recipes/recipes.component';
 
 
 export const fridgeRoutes: Routes = [
     {path:'home',component:HomeComponent},
     {path:'item-edit',component:EditItemComponent},
+    {path:'recipes',component:RecipesComponent},
     {path:'shopping-list',component:ShoppingListComponent},
     {
         path:'admin',
---
+++ src/app/features/fridge/views/recipes/recipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PageTitleComponent } from "../../../../shared/components/atoms/page-title/page-title.component";
+
+@Component({
+  selector: 'app-recipes',
+  imports: [PageTitleComponent],
+  templateUrl: './recipes.component.html',
+  styleUrl: './recipes.component.css'
+})
+export class RecipesComponent {
+
+}
