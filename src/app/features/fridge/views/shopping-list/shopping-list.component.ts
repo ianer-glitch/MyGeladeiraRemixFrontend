aec70014a62ed185ba553eed26845a5794046836
ianer-glitch <ianrepker@gmail.com>
2025-03-08 07:24:31 -0300

    created page-title component

--- src/app/features/fridge/views/shopping-list/shopping-list.component.ts
+++ src/app/features/fridge/views/shopping-list/shopping-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GenericListItemComponent } from "../../../../shared/components/molecules/generic-list-item/generic-list-item.component";
 import { ShoppingListListComponent } from "../../components/shopping-list-list/shopping-list-list.component";
+import { PageTitleComponent } from "../../../../shared/components/atoms/page-title/page-title.component";
 
 @Component({
   selector: 'app-shopping-list',
-  imports: [GenericListItemComponent, ShoppingListListComponent],
+  imports: [GenericListItemComponent, ShoppingListListComponent, PageTitleComponent],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
--- src/app/features/statistic/views/manage-statistic/manage-statistic.component.ts
+++ src/app/features/statistic/views/manage-statistic/manage-statistic.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SustentabilityGraphComponent } from "../../components/sustentability-graph/sustentability-graph.component";
 import { SustentabilityItemRecomendationComponent } from "../../../fridge/components/sustentability-item-recomendation/sustentability-item-recomendation.component";
+import { PageTitleComponent } from "../../../../shared/components/atoms/page-title/page-title.component";
 
 @Component({
   selector: 'app-manage-statistic',
-  imports: [SustentabilityGraphComponent, SustentabilityItemRecomendationComponent],
+  imports: [SustentabilityGraphComponent, SustentabilityItemRecomendationComponent, PageTitleComponent],
   templateUrl: './manage-statistic.component.html',
   styleUrl: './manage-statistic.component.css'
 })
