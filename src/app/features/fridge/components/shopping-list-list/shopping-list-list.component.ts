9b58f659f88347c8e6fa888951c4067ad2f32b20
ianer-glitch <ianrepker@gmail.com>
2025-03-08 14:54:40 -0300

    added loading to shopping list

--- src/app/features/fridge/components/shopping-list-list/shopping-list-list.component.ts
+++ src/app/features/fridge/components/shopping-list-list/shopping-list-list.component.ts
@@ -3,10 +3,11 @@ import { GenericListItemComponent } from "../../../../shared/components/molecule
 import GetItemsShoppingListOut from '../../getItemsShoppingList/GetItemsShoppingListOut';
 import { GetItemsShoppingListService } from '../../getItemsShoppingList/get-items-shopping-list.service';
 import { CommonModule } from '@angular/common';
+import { SkeletonComponent } from "../../../../shared/components/atoms/skeleton/skeleton.component";
 
 @Component({
   selector: 'shopping-list-list',
-  imports: [GenericListItemComponent,CommonModule],
+  imports: [GenericListItemComponent, CommonModule, SkeletonComponent],
   templateUrl: './shopping-list-list.component.html',
   styleUrl: './shopping-list-list.component.css'
 })
@@ -22,9 +23,14 @@ export class ShoppingListListComponent implements OnInit {
   }
 
   listItems : GetItemsShoppingListOut[] = []
-
+  isLoading:boolean = false
   getItemsShoppingList(){
-    this.getItemsShoppingListService.getItemsShoppingList().subscribe(res=>this.listItems = res)
+    this.isLoading = true
+    this.getItemsShoppingListService.getItemsShoppingList().subscribe(res=>
+      {
+        this.listItems = res
+        this.isLoading = false
+      })
   }
 
 }
