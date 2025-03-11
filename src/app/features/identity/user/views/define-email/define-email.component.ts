9525705d2f7d8220f2ddbf526998bbdba1d51cc3
ianer-glitch <ianrepker@gmail.com>
2025-03-11 18:28:25 -0300

    define email translated

--- src/app/features/identity/user/views/define-email/define-email.component.ts
+++ src/app/features/identity/user/views/define-email/define-email.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { DefineEmailFormComponent } from "../../components/define-email-form/define-email-form.component";
 import { StepBarLoginComponent } from "../../components/step-bar-login/step-bar-login.component";
+import { provideTranslocoScope, TranslocoDirective, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-define-email',
-  imports: [DefineEmailFormComponent, StepBarLoginComponent],
+  imports: [DefineEmailFormComponent, StepBarLoginComponent,TranslocoDirective],
   templateUrl: './define-email.component.html',
-  styleUrl: './define-email.component.css'
+  styleUrl: './define-email.component.css',
+  providers:[
+    provideTranslocoScope({
+      scope: "",
+      alias: "mf",
+    }),
+  ]
 })
 export class DefineEmailComponent {
-
+ translocoPath="identity.user.define-email"
 }
