fda77c166431d189580913e5021a2ea2724de10c
ianer-glitch <ianrepker@gmail.com>
2025-03-11 18:43:52 -0300

    translated create-password

--- src/app/features/identity/user/views/create-password/create-password.component.ts
+++ src/app/features/identity/user/views/create-password/create-password.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { CreatePasswordFormComponent } from "../../components/create-password-form/create-password-form.component";
 import { StepBarLoginComponent } from "../../components/step-bar-login/step-bar-login.component";
+import { provideTranslocoScope, TranslocoDirective, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-create-password',
-  imports: [CreatePasswordFormComponent, StepBarLoginComponent],
+  imports: [CreatePasswordFormComponent, StepBarLoginComponent,TranslocoDirective],
   templateUrl: './create-password.component.html',
-  styleUrl: './create-password.component.css'
+  styleUrl: './create-password.component.css',
+  providers:[
+    provideTranslocoScope({
+      scope: "",
+      alias: "mf",
+    }),
+  ]
 })
 export class CreatePasswordComponent {
-
+ translocoPath="identity.user.create-password"
 }
