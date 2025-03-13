5a1deff698af7e4c92be0df122f0644fb6913b9e
ianer-glitch <ianrepker@gmail.com>
2025-03-13 05:18:34 -0300

    added translation in settings

--- src/app/features/identity/user/views/settings/settings.component.ts
+++ src/app/features/identity/user/views/settings/settings.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { PageTitleComponent } from "../../../../../shared/components/atoms/page-title/page-title.component";
-
+import { provideTranslocoScope, TranslocoDirective, TranslocoService } from '@jsverse/transloco';
 @Component({
   selector: 'settings',
-  imports: [PageTitleComponent],
+  imports: [PageTitleComponent,TranslocoDirective],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.css'
+  styleUrl: './settings.component.css',
+  providers:[
+    provideTranslocoScope({
+      scope: "",
+      alias: "mf",
+    }),
+  ]
 })
 export class SettingsComponent {
-
+ translocoPath="identity.user.settings"
 }
